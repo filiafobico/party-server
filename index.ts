3b4be32c153214299d00dbf7ce34effb8815429e
VitorMac10 <vitormac3@hotmail.com>
2020-06-18 23:12:12 -0300

    General purpose events

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ server.on('connection', socket => {
         const rooms = getRooms(socket);
         if (!rooms.length) {
             const room = new Room(socket.id);
-            socket.join(room.id).emit('room', room.id);
+            socket.join(room.id).emit('join-room', room.id);
             Rooms.set(room.id, room);
         } else
             socket.emit('exception', 'You are already connected to a room!');
@@ -28,7 +28,8 @@ server.on('connection', socket => {
             if (!rooms.length) {
                 if (room.connected.length < 4) {
                     room.connected.push(socket.id);
-                    socket.join(room.id).emit('room', room.id);
+                    socket.join(room.id).emit('join-room', room.id);
+                    server.to(room.id).emit('incomer-join', { id: socket.id, name: socket.id });
                 } else
                     socket.emit('exception', 'This room is full!');
             } else
@@ -57,6 +58,7 @@ server.on('connection', socket => {
             room.connected.splice(room.connected.indexOf(socket.id), 1);
 
             if (room.owner === socket.id) {
+                server.to(room.id).emit('leave-room');
                 server.to(room.id).emit('exception', 'The room was closed by the owner!');
                 room.connected.map(e => server.sockets.connected[e])
                     .forEach(client => client.leave(room.id));
