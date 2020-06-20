d502d7fb7ec1353fc190853e6ba96f91a5730900
VitorMac10 <vitormac3@hotmail.com>
2020-06-20 15:51:30 -0300

    Members management

--- index.ts
+++ index.ts
@@ -5,79 +5,84 @@ import * as dotenv from 'dotenv';
 import Member from './models/member';
 
 const Rooms = new Map<string, Room>();
+const Members = new Map<string, Member>();
 
 dotenv.config();
 const server = io(process.env['PORT'] || 3000, { transports: ['websocket'] });
 
 server.on('connection', socket => {
+    socket.on('nickname', (name: string) => {
+        Members.set(socket.id, new Member(name.trim() || 'Unknown', socket));
+    });
+
     socket.on('create', () => {
-        const rooms = getRooms(socket);
+        if (Members.has(socket.id)) {
+            const member = Members.get(socket.id) as Member;
 
-        if (!rooms.length) {
-            const room = new Room({ id: socket.id, name: socket.id });
+            if (!member.room) {
+                const room = new Room(member);
 
-            Rooms.set(room.id, room);
-            server.to(room.id).emit('incomer-join',);
-            socket.join(room.id).emit('join-room', room.id);
-        } else
-            socket.emit('exception', 'You are already connected to a room!');
+                Rooms.set(room.id, room);
+                socket.join(room.id).emit('join-room', room.id);
+            } else {
+                socket.emit('exception', 'You are already connected to a room!');
+            }
+        }
     });
 
     socket.on('join', (code: string) => {
-        if (!Rooms.has(code)) {
-            socket.emit('exception', 'This room does not exist!');
-        } else {
+        if (Members.has(socket.id)) {
+            const member = Members.get(socket.id) as Member;
+
+            if (!Rooms.has(code)) {
+                return socket.emit('exception', 'This room does not exist!');
+            }
+
             const room = Rooms.get(code) as Room;
-            const rooms = getRooms(socket);
-
-            if (!rooms.length) {
+            if (!member.room) {
                 if (room.connected.size < 4) {
-                    const member = { id: socket.id, name: socket.id };
-
                     room.connected.set(socket.id, member);
-                    server.to(room.id).emit('incomer-join', member);
+                    server.to(room.id).emit('incomer-join', { id: member.id, name: member.name });
                     socket.join(room.id).emit('join-room', room.id);
                 } else
                     socket.emit('exception', 'This room is full!');
-            } else
+            } else {
                 socket.emit('exception', 'You are already connected to a room!');
+            }
         }
     });
 
-    socket.on('message', (message: string) => {
-        const rooms = getRooms(socket);
+    socket.on('message', (content: string) => {
+        if (Members.has(socket.id)) {
+            const member = Members.get(socket.id) as Member;
 
-        if (rooms.length == 1 && message.trim()) {
-            server.to(rooms[0]).emit('message', {
-                content: message,
-                author: Rooms.get(rooms[0])?.connected.get(socket.id)
-            });
-        } else if (!rooms.length)
-            socket.emit('exception', 'You are not connected to a room!');
+            if (member.room && content.trim()) {
+                server.to(member.room)
+                    .emit('message', { member: { id: member.id, name: member.name }, content });
+            }
+        }
     });
 
     socket.on('disconnecting', () => {
-        const rooms = getRooms(socket);
+        if (Members.has(socket.id)) {
+            const member = Members.get(socket.id) as Member;
+            Members.delete(socket.id);
 
-        if (rooms.length == 1 && Rooms.has(rooms[0])) {
-            const room = Rooms.get(rooms[0]) as Room;
-            const member = room.connected.get(socket.id) as Member;
+            if (member.room) {
+                const room = Rooms.get(member.room) as Room;
+                room.connected.delete(member.id);
 
-            room.connected.delete(socket.id);
-            if (room.owner === member) {
-                server.to(room.id).emit('leave-room');
-                server.to(room.id).emit('exception', 'The room was closed by the owner!');
+                if (room.owner === member) {
+                    Rooms.delete(room.id);
+                    server.to(room.id).emit('exception', 'The room was closed by the owner!');
 
-                room.connected.forEach((v, k) => {
-                    server.sockets.connected[k].leave(room.id);
-                });
-                Rooms.delete(room.id);
-            } else
-                server.to(room.id).emit('incomer-left', member);
+                    room.connected.forEach((v, k) => {
+                        server.sockets.connected[k].leave(room.id);
+                    });
+                } else
+                    server.to(room.id).emit('incomer-left', { id: member.id, name: member.name });
+            }
         }
     });
-});
 
-function getRooms(socket: Socket) {
-    return Object.keys(socket.rooms).filter(e => e !== socket.id);
-}
+});
--- models/member.ts
+++ models/member.ts
@@ -1,13 +1,31 @@
-export default interface Member {
+import { Socket } from "socket.io";
+
+export default class Member {
 
     /**
      * The member's id
      */
-    id: string;
+    public readonly id: string;
 
     /**
      * The member's name
      */
-    name?: string;
+    public readonly name: string;
+
+    /**
+     * The member's socket
+     */
+    private readonly socket: Socket;
+
+    constructor(name: string, socket: Socket) {
+        this.name = name;
+        this.socket = socket;
+        this.id = this.socket.id;
+    }
+
+    public get room() {
+        return Object.keys(this.socket.rooms)
+            .filter(e => e !== this.socket.id)[0];
+    }
 
 }
